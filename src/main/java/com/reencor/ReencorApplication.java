49e1af96828a8e285dc90d9f083f9e380f3fbba1
15010133689 <691159453@qq.com>
2018-05-08 16:06:42 +0800

    新项目提交

---
+++ src/main/java/com/reencor/ReencorApplication.java
@@ -0,0 +1,12 @@
+package com.reencor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReencorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReencorApplication.class, args);
+	}
+}
