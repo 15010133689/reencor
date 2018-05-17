5117641d872443789c9a8ecc7bffae01a1f1d1ec
15010133689 <691159453@qq.com>
2018-05-17 18:02:29 +0800

    消息中间件

---
+++ parent/p-base/src/main/java/com/reencor/mqutils/MessageConsumer.java
@@ -0,0 +1,19 @@
+package com.reencor.mqutils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+
+public class MessageConsumer implements ChannelAwareMessageListener {
+
+    private Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @Override
+    public void onMessage(Message message, com.rabbitmq.client.Channel channel) throws Exception {
+        byte[] body = message.getBody();
+        System.out.println("收到消息1 : " + new String(body));
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //确认消息成功消费
+
+    }
+}
--- webs/web-display/src/main/java/com/reencor/controller/WebdisplayController.java
+++ webs/web-display/src/main/java/com/reencor/controller/WebdisplayController.java
@@ -1,11 +1,17 @@
 package com.reencor.controller;
 
 import com.google.gson.Gson;
+import com.reencor.configuration.ConnectionFactoryConfigure;
 import com.reencor.display.entity.User;
 import com.reencor.interapi.SVCDisplayInterApi;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
 @RestController
 public class WebdisplayController {
@@ -23,5 +29,16 @@ public class WebdisplayController {
             e.printStackTrace();
         }
     }
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @RequestMapping("/send")
+    public String send3() throws UnsupportedEncodingException {
+        String uuid = UUID.randomUUID().toString();
+        CorrelationData correlationId = new CorrelationData(uuid);
+        rabbitTemplate.convertAndSend(ConnectionFactoryConfigure.EXCHANGE, ConnectionFactoryConfigure.ROUTINGKEY2, "哈哈", correlationId);
+        return uuid;
+    }
+
 
 }
